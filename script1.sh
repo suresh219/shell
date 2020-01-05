52c3921880e2c7542a35591cf265fa0bfeeeb94b
suresh <asureshvkd@gmail.com>
2020-01-05 10:44:22 +0000

    examples

---
+++ script1.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+FILE="/var/lib"
+if [ -f "$FILE" ]
+then 
+    echo " it is a regular file"
+    exit 0
+elif [ -d "$FILE" ]
+then
+    echo " it is directory"
+    exit 1
+else
+    echo "other type of file"
+    exit 2
+fi
