653dba3a1e45d0fceab3b8cb5fdd6e3ddb2a34a9
suresh <asureshvkd@gmail.com>
2020-01-05 12:37:52 +0000

    commit

---
+++ exitstatus.sh
@@ -0,0 +1,12 @@
+cat /var/lib
+if [ "$?" -eq 0 ]then
+  echo "Command Succeeded"
+  exit 0
+else
+  echo " Coomand Failed"
+  exit 1
+fi
+
+
+# not working
+#cat /var/lib && echo "command succeeded" ; echo $? && echo "Command failed"
